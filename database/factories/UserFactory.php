37a7e2b07471ed0d164c46adf6f73c9fb79630a9
AkramAnwarAl-Shamiri1 <akramalshmiri2019@gmail.com>
2025-09-06 22:03:20 +0300

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,7 +16,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => bcrypt('password'), // كلمة مرور افتراضية
+            'password' => bcrypt('password'),
             'phone_number' => $this->faker->phoneNumber(),
             'remember_token' => Str::random(10),
         ];
