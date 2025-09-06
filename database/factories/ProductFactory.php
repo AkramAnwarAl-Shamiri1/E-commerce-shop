1afb34914d5b146eb0d1f9b1e3dfe5cdf48c5da4
AkramAnwarAl-Shamiri1 <akramalshmiri2019@gmail.com>
2025-09-06 23:20:42 +0300

    Update ProductFactory.php

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -15,14 +15,14 @@ class ProductFactory extends Factory
     {
         $name = $this->faker->unique()->words(3, true);
         return [
-            'category_id' => Category::inRandomOrder()->first()->id ?? 1, // رابط مع فئة موجودة
+            'category_id' => Category::inRandomOrder()->first()->id ?? 1,
             'name' => $name,
             'slug' => Str::slug($name),
             'description' => $this->faker->sentence(),
             'price' => $this->faker->randomFloat(2, 10, 500),
             'on_sale' => $this->faker->boolean(30),
             'image' => 'download1.jpg',
- // أو يمكنك توليد اسم صورة وهمية
+
         ];
     }
 }
