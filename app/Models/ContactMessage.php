23c77f0e3c4aac54a64d98a4327d4ad4babbd6ec
AkramAnwarAl-Shamiri1 <akramalshmiri2019@gmail.com>
2025-09-06 22:00:28 +0300

    Update ContactMessage.php

--- app/Models/ContactMessage.php
+++ app/Models/ContactMessage.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactMessage extends Model
 {
-    // اسم الجدول في قاعدة البيانات
+
     protected $table = 'contacts';
 
-    // الحقول التي يمكن تعبئتها جماعياً
+  
     protected $fillable = ['name', 'email', 'message'];
 }
